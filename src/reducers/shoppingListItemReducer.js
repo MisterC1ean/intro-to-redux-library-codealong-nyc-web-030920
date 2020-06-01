3e6b3b95fe25d0a5c1ff5f85023f0da91dae5100
MisterC1ean <robert.keller44@icloud.com>
2020-05-31 23:51:11 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,10 +6,13 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length);
+			console.log('Updating state.items length to %s', state.items.length + 1)
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
+			console.log('Initial state.items length: %s', state.items.length);
 		default:
 			return state;
 	}
